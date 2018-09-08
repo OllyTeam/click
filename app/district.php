0c73fa5c6266cf2edd9f964a15974ef8e1a53a2f
olly <imanishimweolyy@gmail.com>
2018-09-08 20:18:03 +0200

    test

--- app/district.php
+++ app/district.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class district extends Model
 {
-    //
+    protected $fillable = ['name'];
+
+
+
+
+    public function s_listing(){
+        return $this->hasMany('App\s_listing');
+    }
+
+    public function sector(){
+        return $this->hasMany('App\sector');
+    }
 }
--- app/province.php
+++ app/province.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class province extends Model
 {
-    //
+    public function district(){
+        return $this->hasMany('App\district');
+    }
+
+    public function sector(){
+        return $this->hasMany('App\sector');
+    }
 }
